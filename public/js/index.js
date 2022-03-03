d6e384d6e508d607d7889f5f1602184e2bf80bb4
Burak Altıntaş <91549591+buraltintas@users.noreply.github.com>
2022-03-03 15:16:00 +0300

    add file upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 const loginForm = document.querySelector('.form');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
@@ -15,3 +17,15 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+
+    updateSettings(form, 'data');
+  });
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
@@ -15,15 +14,13 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-
-router.get(
-  '/me',
-
-  userController.getMe,
-  userController.getUser
+router.get('/me', userController.getMe, userController.getUser);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
 );
-
-router.patch('/updateMe', userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
